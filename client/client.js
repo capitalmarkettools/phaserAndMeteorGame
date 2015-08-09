f44babff397eefea8ee816f80ed375b29f802be9
summit <capitalmarkettools.org@gmail.com>
2015-08-09 19:47:57 -0400

    Changed to game where car needs to collide with redSquare

--- client/client.js
+++ client/client.js
@@ -1,6 +1,6 @@
 Template.game1.helpers({
     phaserGame: function() {
-	var game = new Phaser.Game(320,320, Phaser.AUTO, 'firstGame', { preload: preload, create: create, update:update }); 
+	var game = new Phaser.Game("80","80", Phaser.AUTO, 'firstGame', { preload: preload, create: create, update:update });
 	return game;
 	
 	function preload() {
@@ -8,12 +8,15 @@ Template.game1.helpers({
 		game.load.image('tinycar', '/tinycar.png');
 		game.load.image('redSquare', '/redSquare25x25.png');
 	}
+
 	function create() {
 	    game.physics.startSystem(Phaser.Physics.P2JS);
 
 	    tinyCars = game.add.group();
-	    for (var i = 0; i < 5; i++) {
-			var tinyCar = tinyCars.create(game.rnd.integerInRange(200, 1700), game.rnd.integerInRange(-200, 400), 'tinycar');
+		numberOfCars = 20;
+	    for (var i = 0; i < numberOfCars; i++) {
+			xPositionStep = game.width/numberOfCars;
+			var tinyCar = tinyCars.create(xPositionStep*i, game.height/2, 'tinycar');
 			game.physics.p2.enable(tinyCar,false);
 	    }
 
@@ -24,42 +27,50 @@ Template.game1.helpers({
 
 		square = game.add.sprite("25", "25", 'redSquare');
 		game.physics.p2.enable(square);
+
+		var won = false;
 	}
 	
 	function update() {
+		//Move tinyCars
 	    tinyCars.forEachAlive(moveTinyCar,this);  //make tinyCars accelerate to car
-	    
+
+		//Move car
 	    if (cursors.left.isDown) {car.body.rotateLeft(100);}   //car movement
 	    else if (cursors.right.isDown){car.body.rotateRight(100);}
 	    else {car.body.setZeroRotation();}
 	    if (cursors.up.isDown){car.body.thrust(400);}
 	    else if (cursors.down.isDown){car.body.reverse(400);}
 
+		//Move square with mouse or tab
 		game.input.onDown.add(moveSquare, this);
 
-/*		if (game.input.pointer1.isDown){
-			if (Math.floor(game.input.x/(game.width/2)) === LEFT) {
-				square.body.velocity.x = 150;
-//				player.animations.play('right');
+		//Check for overlap
+		bounds1 = car.getBounds();
+		bounds2 = square.getBounds();
+
+		if (Phaser.Rectangle.intersects(bounds1, bounds2)) {
+			if (won == false){
+				won = true;
+				alert("You won!!");
+				game.destroy();
 			}
 
-			if (Math.floor(game.input.x/(game.width/2)) === RIGHT) {
-				square.body.velocity.x = -150;
-//				player.animations.play('left');
-			}
-		}else{
-			//  Stand still
-			square.animations.stop();
+
 		}
-		square.body.rotateLeft(10);
-*/
+
+		//Move square randonly
+		square.body.rotateRight(game.rnd.between(0,180));
+		square.body.thrust(game.rnd.between(0,10));
 	}
-		function moveSquare(pointer){
-//			square.body.rotateLeft(25);
-			accelerateToObject(square,pointer, 1000);
-		}
+
+	function moveSquare(pointer){
+//		square.body.rotateLeft(25);
+		accelerateToObject(square,pointer, 5000);
+	}
+
 	function moveTinyCar (tinyCar) {
-	    accelerateToObject(tinyCar,car,30);  //start accelerateToObject on every bullet
+	    accelerateToObject(tinyCar,square,150);  //start accelerateToObject on every bullet
 	}
 
 
@@ -67,8 +78,9 @@ Template.game1.helpers({
 	    if (typeof speed === 'undefined') { speed = 60; }
 	    var angle = Math.atan2(obj2.y - obj1.y, obj2.x - obj1.x);
 	    obj1.body.rotation = angle + game.math.degToRad(90);  // correct angle of angry tinyCars (depends on the sprite used)
-	    obj1.body.force.x = Math.cos(angle) * speed;    // accelerateToObject 
+	    obj1.body.force.x = Math.cos(angle) * speed;    // accelerateToObject
 	    obj1.body.force.y = Math.sin(angle) * speed;
 	}
+
     }
 })
